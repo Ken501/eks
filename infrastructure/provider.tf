b4634c9182254135c0e62eaec18e500c3d08f3ca
kmadmin <kenneth_young501@live.com>
2023-01-15 18:03:10 -0500

    Added provider auth

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -24,3 +24,16 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+// Configure Helm Provider
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.cluster_auth.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.id]
+      command     = "aws"
+    }
+  }
+}
