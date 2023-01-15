1289f34a464be4c4df8470b1d757046b3127c181
kmadmin <kenneth_young501@live.com>
2023-01-15 16:50:45 -0500

    Added ingress class

--- infrastructure/ingress.tf
+++ infrastructure/ingress.tf
@@ -1,66 +1,66 @@
 // Ingress Controller Class
-# resource "kubernetes_ingress_class_v1" "ingress_controller" {
-#   metadata {
-#     name = "${var.environment}-${var.app_name}-ingress-controller"
-#     annotations = {
-#         "kubernetes.io/ingress.class" = "alb"
-#     }
-#   }
+resource "kubernetes_ingress_class_v1" "ingress_controller" {
+  metadata {
+    name = "${var.environment}-${var.app_name}-ingress-controller"
+    annotations = {
+        "kubernetes.io/ingress.class" = "alb"
+    }
+  }
 
+  spec {
+    controller = "example.com/ingress-controller"
+    parameters {
+      kind      = "IngressParameters"
+      name      = "${var.environment}-${var.app_name}-external-alb-${var.AWS_REGION}"
+    }
+  }
+}
+
+# resource "kubernetes_service_v1" "example" {
+#   metadata {
+#     name = "ingress-service"
+#   }
 #   spec {
-#     controller = "example.com/ingress-controller"
-#     parameters {
-#       kind      = "IngressParameters"
-#       name      = "${var.environment}-${var.app_name}-external-alb-${var.AWS_REGION}"
+#     port {
+#       port        = 80
+#       target_port = 80
+#       protocol    = "TCP"
+#     }
+#     type = "NodePort"
+#   }
+# }
+
+# resource "kubernetes_ingress_v1" "example" {
+#   wait_for_load_balancer = true
+#   metadata {
+#     name = "example"
+#   }
+#   spec {
+#     ingress_class_name = "nginx"
+#     rule {
+#       http {
+#         path {
+#           path = "/*"
+#           backend {
+#             service {
+#               name = kubernetes_service_v1.example.metadata.0.name
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
 #     }
 #   }
 # }
 
-resource "kubernetes_service_v1" "example" {
-  metadata {
-    name = "ingress-service"
-  }
-  spec {
-    port {
-      port        = 80
-      target_port = 80
-      protocol    = "TCP"
-    }
-    type = "NodePort"
-  }
-}
+# # Display load balancer hostname (typically present in AWS)
+# output "load_balancer_hostname" {
+#   value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.hostname
+# }
 
-resource "kubernetes_ingress_v1" "example" {
-  wait_for_load_balancer = true
-  metadata {
-    name = "example"
-  }
-  spec {
-    ingress_class_name = "nginx"
-    rule {
-      http {
-        path {
-          path = "/*"
-          backend {
-            service {
-              name = kubernetes_service_v1.example.metadata.0.name
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-# Display load balancer hostname (typically present in AWS)
-output "load_balancer_hostname" {
-  value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.hostname
-}
-
-# Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
-output "load_balancer_ip" {
-  value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.ip
-}
+# # Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
+# output "load_balancer_ip" {
+#   value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.ip
+# }
