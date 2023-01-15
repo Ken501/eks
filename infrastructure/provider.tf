6c7620603b4d82a2bcc6636254b6d32c9568a423
kmadmin <kenneth_young501@live.com>
2023-01-15 01:06:36 -0500

    Added cluster auth token

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -14,22 +14,13 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
-#     command     = "aws"
-#   }
-# }
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster_auth.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "${var.environment}-${var.app_name}-cluster-${var.AWS_REGION}"]
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     command     = "aws"
   }
 }
