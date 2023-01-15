54a3639d086f2fe4a031de057a5896cc14efb230
kmadmin <kenneth_young501@live.com>
2023-01-15 14:27:00 -0500

    added open id connect service account

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -78,8 +78,8 @@ resource "aws_subnet" "public01" {
     var.additional_tags,
     {
        Name = "${var.app_name}-Public-1"
-      #  "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}"  = "owned"
-      #  "kubernetes.io/role/elb" = 1
+       "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}" = "owned"
+       "kubernetes.io/role/elb" = "1"
     },
   )
 }
@@ -94,8 +94,8 @@ resource "aws_subnet" "public02" {
     var.additional_tags,
     {
        Name = "${var.app_name}-Public-2"
-      #  "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}"  = "owned"
-      #  "kubernetes.io/role/elb" = 1
+       "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}" = "owned"
+       "kubernetes.io/role/elb" = "1"
     },
   )
 }
@@ -111,8 +111,8 @@ resource "aws_subnet" "private01" {
     var.additional_tags,
     {
        Name = "${var.app_name}-Private-1"
-      #  "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}"  = "owned"
-      #  "kubernetes.io/role/internal-elb" = 1
+       "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}" = "owned"
+       "kubernetes.io/role/internal-elb" = "1"
     },
   )
 }
@@ -126,8 +126,8 @@ resource "aws_subnet" "private02" {
     var.additional_tags,
     {
        Name = "${var.app_name}-Private-2"
-      #  "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}"  = "owned"
-      #  "kubernetes.io/role/internal-elb" = 1
+       "kubernetes.io/cluster/${aws_eks_cluster.eks_cluster.name}" = "owned"
+       "kubernetes.io/role/internal-elb" = "1"
     },
   )
 }
