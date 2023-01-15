43a558f5620de8b57e7da3c16570b7e737a93e00
kmadmin <kenneth_young501@live.com>
2023-01-14 22:55:22 -0500

    Corrected local invocation

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -23,8 +23,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   role       = aws_iam_role.cluster_role.name
 }
 
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.cluster_role.name
