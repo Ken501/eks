f5f3c2d588b90dfa68788511fc1675094bf7c565
kmadmin <kenneth_young501@live.com>
2023-01-15 17:56:01 -0500

    Fixed misspelling

--- infrastructure/ingress-controller.tf
+++ infrastructure/ingress-controller.tf
@@ -4,7 +4,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.id]
       command     = "aws"
     }
   }
