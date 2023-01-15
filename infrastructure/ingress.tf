33a262da63eb7f6d36262a7987c7d4196568990c
kmadmin <kenneth_young501@live.com>
2023-01-15 16:53:59 -0500

    Added ingress and service

--- infrastructure/ingress.tf
+++ infrastructure/ingress.tf
@@ -16,51 +16,51 @@ resource "kubernetes_ingress_class_v1" "ingress_controller" {
   }
 }
 
-# resource "kubernetes_service_v1" "example" {
-#   metadata {
-#     name = "ingress-service"
-#   }
-#   spec {
-#     port {
-#       port        = 80
-#       target_port = 80
-#       protocol    = "TCP"
-#     }
-#     type = "NodePort"
-#   }
-# }
+resource "kubernetes_service_v1" "example" {
+  metadata {
+    name = "ingress-service"
+  }
+  spec {
+    port {
+      port        = 80
+      target_port = 80
+      protocol    = "TCP"
+    }
+    type = "NodePort"
+  }
+}
 
-# resource "kubernetes_ingress_v1" "example" {
-#   wait_for_load_balancer = true
-#   metadata {
-#     name = "example"
-#   }
-#   spec {
-#     ingress_class_name = "nginx"
-#     rule {
-#       http {
-#         path {
-#           path = "/*"
-#           backend {
-#             service {
-#               name = kubernetes_service_v1.example.metadata.0.name
-#               port {
-#                 number = 80
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_ingress_v1" "example" {
+  wait_for_load_balancer = false
+  metadata {
+    name = "example"
+  }
+  spec {
+    ingress_class_name = "${var.environment}-${var.app_name}-ingress-controller"
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service {
+              name = kubernetes_service_v1.example.metadata.0.name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
-# # Display load balancer hostname (typically present in AWS)
-# output "load_balancer_hostname" {
-#   value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.hostname
-# }
+# Display load balancer hostname (typically present in AWS)
+output "load_balancer_hostname" {
+  value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.hostname
+}
 
-# # Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
-# output "load_balancer_ip" {
-#   value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.ip
-# }
+# Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
+output "load_balancer_ip" {
+  value = kubernetes_ingress_v1.example.status.0.load_balancer.0.ingress.0.ip
+}
