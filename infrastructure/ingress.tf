6b8f0bfbd7a3b77c04c836845d61bfcc8857215d
kmadmin <kenneth_young501@live.com>
2023-01-15 17:33:37 -0500

    Added ingrass oidc and iam role

--- infrastructure/ingress.tf
+++ infrastructure/ingress.tf
@@ -16,48 +16,48 @@
 #   }
 # }
 
-resource "kubernetes_service_v1" "example" {
-  metadata {
-    name = "ingress-service"
-  }
-  spec {
-    port {
-      port        = 80
-      target_port = 80
-      protocol    = "TCP"
-    }
-    type = "NodePort"
-  }
-}
+# resource "kubernetes_service_v1" "example" {
+#   metadata {
+#     name = "ingress-service"
+#   }
+#   spec {
+#     port {
+#       port        = 80
+#       target_port = 80
+#       protocol    = "TCP"
+#     }
+#     type = "NodePort"
+#   }
+# }
 
-resource "kubernetes_ingress_v1" "example" {
-  wait_for_load_balancer = false
-  metadata {
-    name = "example"
-    annotations = {
-        "kubernetes.io/ingress.class" = "alb"
-        "service.beta.kubernetes.io/aws-load-balancer-type" = "alb"
-    }
-  }
-  spec {
-    ingress_class_name = "${var.environment}-${var.app_name}-ingress-controller"
-    rule {
-      http {
-        path {
-          path = "/*"
-          backend {
-            service {
-              name = kubernetes_service_v1.example.metadata.0.name
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+# resource "kubernetes_ingress_v1" "example" {
+#   wait_for_load_balancer = false
+#   metadata {
+#     name = "example"
+#     annotations = {
+#         "kubernetes.io/ingress.class" = "alb"
+#         "service.beta.kubernetes.io/aws-load-balancer-type" = "alb"
+#     }
+#   }
+#   spec {
+#     ingress_class_name = "${var.environment}-${var.app_name}-ingress-controller"
+#     rule {
+#       http {
+#         path {
+#           path = "/*"
+#           backend {
+#             service {
+#               name = kubernetes_service_v1.example.metadata.0.name
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
 # # Display load balancer hostname (typically present in AWS)
 # output "load_balancer_hostname" {
