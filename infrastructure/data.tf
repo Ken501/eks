f429c4407f2f3d4aa8e7efaa97e1910d5cced65a
kmadmin <kenneth_young501@live.com>
2023-01-15 12:42:55 -0500

    Added depends on

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,7 +1,15 @@
 // Cluster Auth for kubernetes provider
 data "aws_eks_cluster" "eks_cluster" {
   name = "${var.environment}-${var.app_name}-cluster-${var.AWS_REGION}"
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
 }
 data "aws_eks_cluster_auth" "cluster_auth" {
   name = "${var.environment}-${var.app_name}-cluster-${var.AWS_REGION}"
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
 }
