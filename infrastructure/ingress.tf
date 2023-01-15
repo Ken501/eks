978a3f7a7d73e58b5a594a4c258cde8be7c86883
kmadmin <kenneth_young501@live.com>
2023-01-15 13:19:00 -0500

    Uncomment ingress controller class

--- infrastructure/ingress.tf
+++ infrastructure/ingress.tf
@@ -1,14 +1,14 @@
-# // Ingress Controller Class
-# resource "kubernetes_ingress_class_v1" "ingress_controller" {
-#   metadata {
-#     name = "${var.environment}-${var.app_name}-ingress-controller"
-#   }
+// Ingress Controller Class
+resource "kubernetes_ingress_class_v1" "ingress_controller" {
+  metadata {
+    name = "${var.environment}-${var.app_name}-ingress-controller"
+  }
 
-#   spec {
-#     controller = "example.com/ingress-controller"
-#     parameters {
-#       kind      = "IngressParameters"
-#       name      = "${var.environment}-${var.app_name}-external-alb-${var.AWS_REGION}"
-#     }
-#   }
-# }
+  spec {
+    controller = "example.com/ingress-controller"
+    parameters {
+      kind      = "IngressParameters"
+      name      = "${var.environment}-${var.app_name}-external-alb-${var.AWS_REGION}"
+    }
+  }
+}
