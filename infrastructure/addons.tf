2160794a735c2b9b273f0a7c905bf8ad94423ff4
kmadmin <kenneth_young501@live.com>
2023-01-20 23:34:34 -0500

    Added correct addon name

--- infrastructure/addons.tf
+++ infrastructure/addons.tf
@@ -1,5 +1,5 @@
 // Cluster Add-ons
 resource "aws_eks_addon" "coredns_addon" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
-  addon_name        = var.coredns_addon_version
+  addon_name        = "coredns"
 }
