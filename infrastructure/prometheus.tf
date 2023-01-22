117faaa81a97e55bba1e4aee27fde94f8e6c15e6
kmadmin <kenneth_young501@live.com>
2023-01-22 15:27:05 -0500

    Modify helm release

--- infrastructure/prometheus.tf
+++ infrastructure/prometheus.tf
@@ -3,16 +3,29 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   namespace  = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "15.5.3"
+  version    = "19.0.0"
 
-  values = [
-    templatefile("${path.module}/templates/prometheus-values.yaml", {
-      pod_security_enabled             = true
-      server_persistent_volume_enabled = false
-      server_resources_limits_cpu      = "256m"
-      server_resources_limits_memory   = "256Mi"
-      server_resources_requests_cpu    = "256m"
-      server_resources_requests_memory = "256Mi"
+  set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
+
+  set {
+    name = "server\\.resources"
+    value = yamlencode({
+      limits = {
+        cpu    = "200m"
+        memory = "50Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "30Mi"
+      }
     })
-  ]
+  }
 }
