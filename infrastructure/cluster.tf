17bccd2e2e9fa27e624409f34ccbf052d68106df
kmadmin <kenneth_young501@live.com>
2023-01-22 10:58:29 -0500

    Enabled cluster public endpoint

--- infrastructure/cluster.tf
+++ infrastructure/cluster.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   vpc_config {
     subnet_ids = local.public_subnet_ids[*]
-    endpoint_private_access = true
+    endpoint_public_access = true
   }
 
   depends_on = [
