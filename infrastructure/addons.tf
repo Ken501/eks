6e5951ced10c376f95cc676fad9d4ac42df95b92
kmadmin <kenneth_young501@live.com>
2023-02-04 14:26:02 -0500

    Enable ebs addon

--- infrastructure/addons.tf
+++ infrastructure/addons.tf
@@ -1,5 +1,13 @@
 // Cluster Add-ons
+
+//CoreDNS
 resource "aws_eks_addon" "coredns_addon" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "coredns"
+}
+
+// EBS
+resource "aws_eks_addon" "ebs_addon" {
+  cluster_name      = aws_eks_cluster.eks_cluster.name
+  addon_name        = "aws-ebs-csi-driver"
 }
