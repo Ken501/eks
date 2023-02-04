05c4e7790c1f41768e3f7d6969fcaeb4a94361b8
kmadmin <kenneth_young501@live.com>
2023-02-04 14:45:05 -0500

    Attached ebs csi policy

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -150,3 +150,39 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_autoscaler_attach" {
 output "eks_cluster_autoscaler_arn" {
   value = aws_iam_role.eks_cluster_autoscaler.arn
 }
+
+// EBS Worker Node Policy
+resource "aws_iam_policy" "eks_worknode_ebs_policy" {
+  name = "Amazon_EBS_CSI_Driver"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteTags",
+        "ec2:DeleteVolume",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+# And attach the new policy
+resource "aws_iam_role_policy_attachment" "worknode-AmazonEBSCSIDriver" {
+  policy_arn = aws_iam_policy.eks_worknode_ebs_policy.arn
+  role       = aws_iam_role.node_role.name
+}
