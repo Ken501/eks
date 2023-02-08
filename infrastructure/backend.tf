15f4eafb3bbc64b57844a9450b4abe57a00d98be
kmadmin <kenneth_young501@live.com>
2023-02-08 15:01:27 -0500

    Modified backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,12 +1,4 @@
 // Configure terraform backend
-# terraform {
-#   backend "s3" {
-#     bucket = var.backend_bucket
-#     key    = "${var.environment}/${var.app_name}/infrastructure/${var.AWS_REGION}"
-#     region = var.AWS_REGION
-#   }
-# }
-
 terraform {
   backend "s3" {
     bucket = "kmartinez-projects"
