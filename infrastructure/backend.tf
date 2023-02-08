c8394709ef3c850192783afc461c5f50a387baf0
kmadmin <kenneth_young501@live.com>
2023-02-07 21:23:08 -0500

    Removed custom action

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
-# // Configure terraform backend
-# terraform {
-#   backend "s3" {
-#     bucket = var.backend_bucket
-#     key    = "${var.environment}/${var.app_name}/infrastructure/${var.AWS_REGION}"
-#     region = var.AWS_REGION
-#   }
-# }
+// Configure terraform backend
+terraform {
+  backend "s3" {
+    bucket = var.backend_bucket
+    key    = "${var.environment}/${var.app_name}/infrastructure/${var.AWS_REGION}"
+    region = var.AWS_REGION
+  }
+}
