895cb0a12c61d497590b8a51009c1609a3559a0a
kmadmin <kenneth_young501@live.com>
2023-02-10 11:41:05 -0500

    Test

--- infrastructure/addons.tf
+++ infrastructure/addons.tf
@@ -9,5 +9,5 @@ resource "aws_eks_addon" "coredns_addon" {
 // EBS
 resource "aws_eks_addon" "ebs_addon" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
-  addon_name        = "aws-ebs-csi-driver"
+  #addon_name        = "aws-ebs-csi-driver"
 }
