a3173a5e0ae5d59d69591dd89449817074a4cfaf
kmadmin <kenneth_young501@live.com>
2023-02-10 10:57:40 -0500

    Revert backend changes

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,16 +1,8 @@
 // Configure terraform backend
-# terraform {
-#   backend "s3" {
-#     bucket = var.backend_bucket
-#     key    = "${var.environment}/${var.app_name}/infrastructure/${var.AWS_REGION}"
-#     region = var.AWS_REGION
-#   }
-# }
-
 terraform {
   backend "s3" {
-    bucket = locals.backend_bucket
-    key    = locals.key
-    region = locals.region
+    bucket = "kmartinez-projects"
+    key    = "infrastructure/us-east-1"
+    region = "us-east-1"
   }
 }
