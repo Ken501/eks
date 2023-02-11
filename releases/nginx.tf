6f86eed8b68ff20ea8b79c3c4cd71ec74e7fec39
kmadmin <kenneth_young501@live.com>
2023-02-11 11:52:20 -0500

    Fixed incorrect repo url

--- releases/nginx.tf
+++ releases/nginx.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx" {
   chart      = "nginx"
   name       = "nginx-deploy"
   namespace  = "${var.environment}-${var.app_name}-ns-${var.AWS_REGION}"
-  repository = "https://https://ken501.github.io/gitchartrepo/"
+  repository = "https://ken501.github.io/gitchartrepo/"
   version    = "0.1.0"
 
 }
