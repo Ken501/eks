5fe5ebc7a53d5fa06e89c4091593697f1c514e9b
kmadmin <kenneth_young501@live.com>
2023-02-11 10:45:28 -0500

    Added kubernetes provider

--- releases/provider.tf
+++ releases/provider.tf
@@ -13,6 +13,17 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+// Configure the Kubernetes Provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.id]
+    command     = "aws"
+  }
+}
 // Configure the Helm Provider
 provider "helm" {
   kubernetes {
