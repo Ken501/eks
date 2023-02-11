1be1702f877c5308c67386b6f3e812b7c7f387cb
kmadmin <kenneth_young501@live.com>
2023-02-11 10:40:38 -0500

    Modified helm release

--- releases/nginx.tf
+++ releases/nginx.tf
@@ -1,6 +1,6 @@
-resource "helm_release" "prometheus" {
+resource "helm_release" "nginx" {
   chart      = "nginx"
-  name       = "nginx"
+  name       = "nginx-deploy"
   namespace  = "${var.environment}-${var.app_name}-ns-${var.AWS_REGION}"
   repository = "https://https://ken501.github.io/gitchartrepo/"
   version    = "0.1.0"
